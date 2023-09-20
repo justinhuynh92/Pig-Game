febbd8050c8253354d481166b0380180e12a5f48
Justin Huynh <justinhuynh639@gmail.com>
2023-09-20 15:59:49 -0700

    fixed while loop

--- project1.py
+++ project1.py
@@ -8,5 +8,17 @@ def roll():
 
     return roll
 
-value = roll()
-print(value)
+#enter the required number of players using a while loop
+while True:
+    players = input("Enter the number of players (2 - 4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Must be between 2 - 4 players.")
+    #error handling
+    else:
+        print("Invalid, try again.")
+
+print(players)
